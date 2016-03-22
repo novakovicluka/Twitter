9792916b1c78aa4b29f84f9a97056a93e7f09e12
Luka <Luka@Luka-PC>
2016-03-22 15:39:36 +0100

    Dodat je javadoc za svaki atribut/metodu iz klase TwitterPoruka

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,27 +1,55 @@
 package com.twitter.poruke;
 
 public class TwitterPoruka {
-
+/**
+ * atribut koji opisuje koji korisnik ostavlja poruku na tviter nalogu
+ */
 	private String korisnik;
+	/**
+	 * atribut koji nam pokazuje poruku koju je ostavio korisnik na tviter nalogu
+	 */
+	
 	private String poruka;
+	
+	/**
+	 * metoda koja vraca korisnika koji je ostavio odredjenu poruku
+	 * @return korisnik
+	 */
 	public String getKorisnik() {
 	return korisnik;
 	}
+	/**
+	 * metoda koja postavlja novu vrednost korisnika
+	 * @param korisnik
+	 */
+	
 	public void setKorisnik(String korisnik) {
 	if (korisnik==null || korisnik == "")
 	throw new RuntimeException(
 	"Ime korisnika mora biti uneto");
 	this.korisnik = korisnik;
 	}
+	
+	/**
+	 * metoda koja vraca poruku 
+	 * @return poruka
+	 */
 	public String getPoruka() {
 	return "poruka";
 	}
+	/**
+	 * metoda koja postavlja novu vrednost poruke
+	 * @param poruka
+	 */
 	public void setPoruka(String poruka) {
 	if (poruka==null || this.poruka.length()>140)
 	throw new RuntimeException(
 	"Poruka mora biti uneta i mora imati najvise 140 znakova");
 	this.poruka = poruka;
 	}
+	/**
+	 * metoda toString koja vraca u celosti koji je korisnik postavio koju poruku u obliku jedne recenice tj stringa
+	 */
 	public String toString(){
 	return "KORISNIK:"+korisnik+" PORUKA:"+poruka;
 	} 
