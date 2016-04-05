fa7cf208c2635e33988fadfb130aeebdde876a15
Luka <lukanovakovic2@gmail.com>
2016-04-05 23:35:16 +0200

    Ispavljene greske u Twitter klasi

--- Twitter/src/com/twitter/Twitter.java
+++ Twitter/src/com/twitter/Twitter.java
@@ -63,7 +63,7 @@ public class Twitter {
 		for (int i = 0; i < poruke.size(); i++)
 		if (poruke.get(i).getPoruka().indexOf(tag)!=-1)
 		if (brojac < maxBroj){
-		rezultat[brojac+1]=poruke.get(i);
+		rezultat[brojac]=poruke.get(i);
 		brojac++;
 		}
 		else break; 
