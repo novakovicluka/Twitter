bda8b285602800efb708280bac366f028c5d4007
Luka <lukanovakovic2@gmaill.com>
2016-04-05 23:05:34 +0200

    Ispravljene odredjene greske u klasi TwitterPoruka

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -21,6 +21,12 @@ public class TwitterPoruka {
 	/**
 	 * metoda koja postavlja novu vrednost korisnika
 	 * @param korisnik
+	 * @throws java.lang.RuntimeException ako je uneti korisnik:
+	 * <ul>
+	 * 	<li>null vrednost
+	 * 	<li>prazan string
+	 * </ul>
+
 	 */
 	
 	public void setKorisnik(String korisnik) {
@@ -40,15 +46,22 @@ public class TwitterPoruka {
 	/**
 	 * metoda koja postavlja novu vrednost poruke
 	 * @param poruka
+	 * @throws java.lang.RuntimeException ako je uneta poruka:
+	 * <ul>
+	 * 	<li>null vrednost
+	 * 	<li>string duzine vece od 140 karaktera
+	 * </ul>
+
 	 */
 	public void setPoruka(String poruka) {
-	if (poruka==null || this.poruka.length()>140)
+	if (poruka==null || poruka.length()>140)
 	throw new RuntimeException(
 	"Poruka mora biti uneta i mora imati najvise 140 znakova");
 	this.poruka = poruka;
 	}
 	/**
 	 * metoda toString koja vraca u celosti koji je korisnik postavio koju poruku u obliku jedne recenice tj stringa
+	 * 
 	 */
 	public String toString(){
 	return "KORISNIK:"+korisnik+" PORUKA:"+poruka;
